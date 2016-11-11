591e678f9a41beaa1aa3a9183b0d91e63899db37
Justas <justas@Justass-MacBook-Pro.local>
2016-11-11 02:25:56 +0000

    Added a backend middleman interface using Retrofit

--- app/src/main/java/com/example/justas/grabble/ServerService.java
+++ app/src/main/java/com/example/justas/grabble/ServerService.java
@@ -1,8 +1,33 @@
 package com.example.justas.grabble;
 
-/**
- * Created by justas on 11/10/16.
- */
+import android.util.Log;
 
-public class ServerService {
+import java.io.IOException;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.Retrofit;
+import retrofit2.http.GET;
+
+public final class ServerService {
+    public static final String API_URL = "https://grabble-justasb.herokuapp.com/";
+
+    public interface Placemarks {
+        @GET("/placemarks")
+        Call<List<MarkerItem>> getPlacemarks();
+    }
+
+    public static void getAllPlacemarks(Callback<List<MarkerItem>> callback) throws IOException {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(API_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        Placemarks placemarks = retrofit.create(Placemarks.class);
+        Call<List<MarkerItem>> call = placemarks.getPlacemarks();
+        call.enqueue(callback);
+    }
 }
