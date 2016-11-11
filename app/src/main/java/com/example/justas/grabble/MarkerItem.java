4bf9e65b2dc025746a1812df7f576600187d1566
Justas <justas@Justass-MacBook-Pro.local>
2016-11-11 02:26:33 +0000

    Update MarkerItem for GSON conversion

--- app/src/main/java/com/example/justas/grabble/MarkerItem.java
+++ app/src/main/java/com/example/justas/grabble/MarkerItem.java
@@ -4,25 +4,26 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.maps.android.clustering.ClusterItem;
 
 public class MarkerItem implements ClusterItem {
-    private LatLng latLng;
-    private String label;
+    public String letter;
+    public Double lat;
+    public Double lng;
 
-    public MarkerItem(String label, double lat, double lng) {
-        this.label = label;
-        this.latLng = new LatLng(lat, lng);
-    }
-
-    public MarkerItem(String label, LatLng latLng) {
-        this.label = label;
-        this.latLng = latLng;
+    public MarkerItem(String letter, double lat, double lng) {
+        this.letter = letter;
+        this.lat = lat;
+        this.lng = lng;
     }
 
     @Override
     public LatLng getPosition() {
-        return latLng;
+        if (lat != null && lng != null){
+            return new LatLng(lat, lng);
+        } else {
+            return null;
+        }
     }
 
     public String getLabel() {
-        return label;
+        return letter;
     }
 }
