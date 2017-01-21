9e4fd4b32aa5bc72b9d6519321559ef276add326
Justas <justas@Justass-MBP.lan>
2017-01-21 14:49:38 +0200

    Added few helper classes for user registration

--- app/src/main/java/com/example/justas/grabble/Player.java
+++ app/src/main/java/com/example/justas/grabble/Player.java
@@ -1,8 +1,37 @@
 package com.example.justas.grabble;
 
 public class Player {
-    public String name;
-    public int totalPoints;
+    private String name;
+    private String id;
+    private int totalPoints;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getTotalPoints() {
+        return totalPoints;
+    }
+
+    public void setTotalPoints(int totalPoints) {
+        this.totalPoints = totalPoints;
+    }
+
+    public Player() {
+    }
 
     public Player(String name, int totalPoints) {
         this.name = name;
