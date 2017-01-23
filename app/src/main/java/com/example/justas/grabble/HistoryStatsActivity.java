008021d9740a625f25427fd44ad4df140b888491
Justas <justas@Justass-MacBook-Pro.local>
2017-01-23 17:22:32 +0000

    Improvements to stats UI, new cursorAdaptor for word history

--- app/src/main/java/com/example/justas/grabble/HistoryStatsActivity.java
+++ app/src/main/java/com/example/justas/grabble/HistoryStatsActivity.java
@@ -1,17 +1,25 @@
 package com.example.justas.grabble;
 
+import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.format.DateUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.CursorAdapter;
 import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
 import com.example.justas.grabble.data.SubmittedWordsContract.WordEntry;
 import com.example.justas.grabble.data.SubmittedWordsOpenHelper;
 import com.example.justas.grabble.helper.ScoredWord;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class HistoryStatsActivity extends AppCompatActivity {
     private ListView mSubmittedWordsListView;
@@ -50,11 +58,44 @@ public class HistoryStatsActivity extends AppCompatActivity {
         }
 
         Cursor cursor = fetchAllSubmittedWords();
-        SimpleCursorAdapter cursorAdapter = new SimpleCursorAdapter(getApplicationContext(), R.layout.submitted_words_list_item, cursor,
-                new String[]{WordEntry.COLUMN_NAME_WORD, WordEntry.COLUMN_NAME_SCORE, WordEntry.COLUMN_NAME_DATETIME},
-                new int[]{R.id.submitted_word_item_word, R.id.submitted_word_item_score, R.id.submitted_word_item_datetime},
-                CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
+
+        CursorAdapter cursorAdapter = new CursorAdapter(getApplicationContext(), cursor, CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER) {
+            @Override
+            public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
+                return LayoutInflater.from(context).inflate(R.layout.submitted_words_list_item, viewGroup, false);
+            }
+
+            @Override
+            public void bindView(View view, Context context, Cursor cursor) {
+                TextView wordView = (TextView) view.findViewById(R.id.submitted_word_item_word);
+                TextView scoreView = (TextView) view.findViewById(R.id.submitted_word_item_score);
+                TextView dateView = (TextView) view.findViewById(R.id.submitted_word_item_datetime);
+
+                String word = cursor.getString(cursor.getColumnIndex(WordEntry.COLUMN_NAME_WORD));
+                String score = cursor.getString(cursor.getColumnIndex(WordEntry.COLUMN_NAME_SCORE));
+                String date = cursor.getString(cursor.getColumnIndex(WordEntry.COLUMN_NAME_DATETIME));
+                String userFriendlyDate = getUserFriendlyDate(date);
+
+                wordView.setText(word);
+                scoreView.setText(score);
+                dateView.setText(userFriendlyDate);
+            }
+        };
+
         mSubmittedWordsListView.setAdapter(cursorAdapter);
+    }
+
+    private String getUserFriendlyDate(String date) {
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        try {
+            long time = df.parse(date).getTime();
+            date = String.valueOf(DateUtils.getRelativeDateTimeString(getApplicationContext(), time,
+                    DateUtils.SECOND_IN_MILLIS, DateUtils.WEEK_IN_MILLIS, DateUtils.FORMAT_NO_NOON));
+            return date;
+        } catch (ParseException e) {
+            return date;
+        }
     }
 
     private Cursor fetchAllSubmittedWords() {
